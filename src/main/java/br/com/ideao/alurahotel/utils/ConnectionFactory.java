b1515352ee20e61f868360042f5a61fb6febc9cb
Wellington Ideão <wellington.ideao@gmail.com>
2023-08-22 21:07:58 -0300

    Add Pool de conexoes c3p0

--- src/main/java/br/com/ideao/alurahotel/utils/ConnectionFactory.java
+++ src/main/java/br/com/ideao/alurahotel/utils/ConnectionFactory.java
@@ -1,15 +1,34 @@
 package br.com.ideao.alurahotel.utils;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
+	
+	private DataSource dataSource;
+	
+	public ConnectionFactory() {
+		ComboPooledDataSource cpds = new ComboPooledDataSource();
+		cpds.setJdbcUrl("jdbc:mysql://localhost/hotel_alura?useTimezone=true&serverTimezone");
+		cpds.setUser("root");
+		cpds.setPassword("dbsql123");
+		
+		cpds.setMinPoolSize(3);
+		cpds.setMaxPoolSize(100);
+		cpds.setMaxIdleTime(300);
+		cpds.setMaxStatements(50);
+		cpds.setIdleConnectionTestPeriod(120);
+		
+		this.dataSource = cpds;
+	}
 
 	public Connection getConnection() {
 		try {
-			return DriverManager.getConnection("jdbc:mysql://localhost/hotel_alura?" +
-			"useTimezone=true&serverTimezone","root", "dbsql123");
+			return this.dataSource.getConnection();
 		} catch (SQLException e) {
 			throw new RuntimeException(e); 
 		} 
